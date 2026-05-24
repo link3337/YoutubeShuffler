724d7ca40d610a4189956cc6f2c7bb890a63635e
link3337 <marcel.graf33@gmail.com>
2026-05-24 03:33:58 +0200

    feat: display version

--- src/components/routes/AboutRoute.tsx
+++ src/components/routes/AboutRoute.tsx
@@ -1,30 +1,34 @@
 import { Anchor, List, Paper, Stack, Text, Title } from '@mantine/core';
+import packageJson from '../../../package.json';
 
 const TESTERS = ['Homura'];
 
 export default function AboutRoute() {
-  return (
-    <Paper withBorder radius="xl" p="lg" className="panel">
-      <Stack gap="sm">
-        <Title order={1} mb={6} className="title">
-          About
-        </Title>
+    return (
+        <Paper withBorder radius="xl" p="lg" className="panel">
+            <Stack gap="sm">
+                <Title order={1} mb={6} className="title">
+                    About
+                </Title>
 
-        <Text c="dimmed">YouTube Playlist Shuffler by link3337.</Text>
+                <Text c="dimmed">YouTube Playlist Shuffler by link3337.</Text>
+                <Text size="sm" c="dimmed">
+                    Version: {packageJson.version}
+                </Text>
 
-        <Anchor href="https://github.com/link3337/YoutubeShuffler" target="_blank" rel="noreferrer">
-          Repository
-        </Anchor>
+                <Anchor href="https://github.com/link3337/YoutubeShuffler" target="_blank" rel="noreferrer">
+                    Repository
+                </Anchor>
 
-        <Text fw={600} mt="xs">
-          Testers
-        </Text>
-        <List spacing="xs" size="sm">
-          {TESTERS.map((tester) => (
-            <List.Item key={tester}>{tester}</List.Item>
-          ))}
-        </List>
-      </Stack>
-    </Paper>
-  );
+                <Text fw={600} mt="xs">
+                    Testers
+                </Text>
+                <List spacing="xs" size="sm">
+                    {TESTERS.map((tester) => (
+                        <List.Item key={tester}>{tester}</List.Item>
+                    ))}
+                </List>
+            </Stack>
+        </Paper>
+    );
 }
